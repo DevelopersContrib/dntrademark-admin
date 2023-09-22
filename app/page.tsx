57745eb55170287aafcba40819ac615bd9a0088d
Stephen <dev.scatacte@gmail.com>
2023-09-22 21:33:31 +0800

    authorization signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import ECommerce from "@/components/Dashboard/E-commerce";
-import { Metadata } from "next";
+import { Metadata } from 'next';
+
+import ECommerce from '@/components/Dashboard/E-commerce';
+import Loader from '@/components/common/Loader';
 
 export const metadata: Metadata = {
-  title: "TailAdmin | Next.js E-commerce Dashboard Template",
-  description: "This is Home Blog page for TailAdmin Next.js",
+  title: 'TailAdmin | Next.js E-commerce Dashboard Template',
+  description: 'This is Home Blog page for TailAdmin Next.js',
   // other metadata
 };
 
 export default function Home() {
   return (
     <>
+      {/* <Loader /> */}
       <ECommerce />
     </>
   );
---
+++ declarations.d.ts
@@ -0,0 +1,1 @@
+declare function atob(encodedString: String): string;
