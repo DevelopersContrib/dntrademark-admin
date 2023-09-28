3579b0dda7964fa9d9b55acfcc43ca963a4cd6f5
jaysoncano <cano.jayson10@gmail.com>
2023-09-28 14:28:51 +0800

    feat: Add loading if pricing data is not complete

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,9 +2,20 @@
 import { PackagesProps } from "@/types/packages";
 import { useEffect, useState } from "react";
 import Packages from "@/components/Pricing/Packages";
+import { Metadata } from 'next';
+import { FaCircleNotch } from 'react-icons/fa6'
+
+export const metadata: Metadata = {
+  title: 'TailAdmin | Next.js E-commerce Dashboard Template',
+  description: 'This is Home Blog page for TailAdmin Next.js',
+  // other metadata
+};
+
 const Page = () => {
 
   const [plans, setPlans] = useState<PackagesProps[]>([]);
+
+  const [loading, setLoading] = useState(true);
 
   const getPackages = async () => {
     try {
@@ -17,12 +28,14 @@ const Page = () => {
 
       const result = await res.json();
 
-      setPlans(result.data)
+      setPlans(result.data);
+      setLoading(false);
 
       // console.log(result.data);
 
     } catch (error) {
       console.log(`Error: ${error}`);
+      setLoading(false);
     }
   }
 
@@ -33,13 +46,19 @@ const Page = () => {
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        {/* Start:: Pricing Item */}
-        {plans.map((plan: PackagesProps) => (
-          <Packages key={plan.id} {...plan} />
-        ))}
-
-      </div>
+      {/* Start:: Pricing Item */}
+      {
+        loading ?
+          <div className="w-full min-h-[50vh] flex items-center justify-center">
+            <FaCircleNotch className="fa-spin text-4xl text-[rgb(117_118_147)]" />
+          </div>
+          :
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+            {plans.map((plan: PackagesProps) => (
+              <Packages key={plan.id} {...plan} />
+            ))}
+          </div>
+      }
     </>
   )
 }
