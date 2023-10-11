0e259145c680a94c800b6484d9198e2a99a03a9e
ron <row_none@yahoo.com>
2023-10-10 21:21:37 -0700

    v0.11.3

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,7 +4,6 @@ import StripeWrapper from '@/components/Checkout/StripeWrapper';
 export const metadata: Metadata = {
   title: 'DNTrademark Admin - Global Trademark Notification Platform',
   description: 'dntrademark.com is a SaaS platform designed to provide an efficient and user-friendly way to check domain names against global trademark databases.',
-  // other metadata
 };
 
 export default function App() {
