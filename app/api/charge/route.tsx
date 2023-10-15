463bb51097c112289fafc6b469a6f7190649d11f
ron <row_none@yahoo.com>
2023-10-15 14:54:18 -0700

    checkout page with stripe

---
+++ app/api/charge/route.tsx
@@ -0,0 +1,44 @@
+//https://sultanoveli.medium.com/how-to-add-stripe-payments-to-your-next-js-app-d1cfced7c8a5
+import { NextResponse, NextRequest } from "next/server";
+import Stripe from 'stripe';
+import { getPackage } from '@/lib/data';
+import { options } from '@/lib/options';
+import { getServerSession } from "next-auth/next"
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2023-08-16',
+  typescript: true
+});
+
+export const POST = async (req: Request) => {
+  try {
+    const session = await getServerSession(options)
+
+    const data = await req.json();
+    const pack = await getPackage(parseInt(data.pack_id));
+    const amount = Math.round(pack.price*100); 
+   
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "USD",
+        });
+
+        const data = {
+            email: session?.user?.email,
+            amount: amount,
+            prince:pack.price,
+            client_secret:paymentIntent.client_secret
+        }
+        console.log('data',data); //
+        return new NextResponse(paymentIntent.client_secret, { status: 200 });
+    } catch (error: any) {
+        return new NextResponse(error, {
+        status: 400,
+        });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    env: {
+        STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    },
     images: {
         remotePatterns: [
             {
