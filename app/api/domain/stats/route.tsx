7a9832da7809a144089b94c27936e8eed9124423
Kareen <kjabellar@gmail.com>
2023-10-18 08:01:43 +0800

    show domain stats

---
+++ app/api/domain/stats/route.tsx
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+import { options } from '@/lib/options';
+import { getServerSession } from "next-auth/next"
+
+export const GET = async (req: Request) => {
+  try {
+
+    const session = await getServerSession(options)
+
+    const config = {
+      headers:{ 'Authorization': 'Bearer '+session?.token }
+    };
+    
+    const apiUrl = process.env.API_URL + '/domains/stats?api_key=' + process.env.API_KEY;
+
+    
+    const res = await axios.get(apiUrl, config)
+    const result = res.data.data;
+   
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
