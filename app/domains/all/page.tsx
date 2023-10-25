11f392a57d6304c67bd13113d5ed5485930f0c6f
ron <row_none@yahoo.com>
2023-10-24 19:29:37 -0700

    dynamic domains.

--- app/domains/all/page.tsx
+++ app/domains/all/page.tsx
@@ -1,8 +1,16 @@
 import AllDomains from "@/components/Domains/AllDomains"
+import {getDomainList} from '@/lib/data'
 
-const page = () => {
+// import { domains } from "@/types/domains";
+import { domainTable } from "@/types/domainTable";
+
+const page = async() => {
+  const domainlist = await getDomainList();
+  
+  const tData = domainlist as domainTable;
+
   return (
-    <AllDomains />
+    <AllDomains tData={tData} />
   )
 }
 
