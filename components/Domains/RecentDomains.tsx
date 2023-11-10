fb15a956b5ddba2d8c28dcf82d2ded7e6095afbb
jaysoncano <cano.jayson10@gmail.com>
2023-11-10 13:45:14 +0800

    Style: Dashoard - update widgets - image, icons, spacing, and sizes.

--- components/Domains/RecentDomains.tsx
+++ components/Domains/RecentDomains.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { domains } from "@/types/domains";
 import Link from "next/link";
+import { BsDatabaseCheck } from "react-icons/bs";
 interface domainProps {
   domains: domains[];
 }
@@ -8,31 +9,29 @@ interface domainProps {
 const RecentDomains = ({ domains }: domainProps) => {
   return (
     <>
-        <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-          <h4 className="mb-6 text-xl font-bold text-black dark:text-white">Recent Domain Uploads</h4>
-          
-            {
-              domains.map((item)=>(
-                <div key={item.id} className="grid grid-cols-2 border-b border-t border-stroke dark:border-strokedark sm:grid-cols-4">
-                <div className="flex items-center gap-3 p-2.5 xl:p-5">
-                        <div className="h-8 w-8 max-w-8 flex-shrink-0">
-                        <svg className="h-8 w-8 text-red-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17a5 5 0 01-.916-9.916 5.002 5.002 0 019.832 0A5.002 5.002 0 0116 17m-7-5l3-3m0 0l3 3m-3-3v12"/>
-                        </svg>
+      <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark flex flex-col">
+        <h4 className="mb-6 text-xl font-bold text-black dark:text-white">
+          Recent Domain Uploads
+        </h4>
 
-                        </div>
-                        <p className="hidden font-medium text-black dark:text-white sm:block">
-                        {item.domain_name}
-                        </p>
-                  </div>      
-                  
-                </div>
-              ))
-            }
-         
-        </div>
+        {domains.map((item) => (
+          <div
+            key={item.id}
+            className="flex w-full flex-col last:border-b border-t border-stroke dark:border-strokedark"
+          >
+            <div className="flex items-center p-2.5 xl:p-5">
+              <div className=" mr-3 flex-shrink-0">
+                <BsDatabaseCheck className="h-5 w-5" />
+              </div>
+              <p className="hidden font-medium text-black dark:text-white sm:block">
+                {item.domain_name}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default RecentDomains
+export default RecentDomains;
