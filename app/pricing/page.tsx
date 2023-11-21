2761d80dc7a961c0adaff67ecdac63d939a870dd
Stephen <dev.scatacte@gmail.com>
2023-11-21 09:44:00 +0800

    .

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,67 +1,3 @@
-/*
-"use client"
-import { PackagesProps } from "@/types/packages";
-import { useEffect, useState } from "react";
-import Packages from "@/components/Pricing/Packages";
-import { Metadata } from 'next';
-import { FaCircleNotch } from 'react-icons/fa6'
-
-const Page = () => {
-
-  const [plans, setPlans] = useState<PackagesProps[]>([]);
-
-  const [loading, setLoading] = useState(true);
-
-  const getPackages = async () => {
-    try {
-      const res = await fetch('api/package', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      const result = await res.json();
-
-      setPlans(result.data);
-      setLoading(false);
-
-      // console.log(result.data);
-
-    } catch (error) {
-      console.log(`Error: ${error}`);
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    getPackages();
-  }, []);
-
-
-  return (
-    <>
-      {// Start:: Pricing Item}
-      {
-        loading ?
-          <div className="w-full min-h-[50vh] flex items-center justify-center">
-            <FaCircleNotch className="fa-spin text-4xl text-[rgb(117_118_147)]" />
-          </div>
-          :
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-            {plans.map((plan: PackagesProps) => (
-              <Packages key={plan.id} {...plan} />
-            ))}
-          </div>
-      }
-    </>
-  )
-}
-
-export default Page;
-
-*/
-
 import { getPackages } from '@/lib/data';
 import { PackagesProps } from "@/types/packages";
 import Packages from "@/components/Pricing/Packages";
@@ -71,12 +7,13 @@ async function Page() {
   const plans = await getPackages();
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 mb-4">
+      {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 mb-4">
         {plans.data.map((plan: PackagesProps) => (
             <Packages key={plan.id} {...plan} />
         ))}
-      </div>
+      </div> */}
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
+        {/* Plan 1 */}
         <div className="group relative bg-white dark:bg-black rounded-lg shadow-solid-10 dark:shadow-none border border-stroke dark:border-black p-7.5 xl:p-12.5">
           <h2 className="mb-5 text-[32px] font-bold text-black dark:text-white">
             Explorer Plan
@@ -194,9 +131,9 @@ async function Page() {
             Current
           </button>
         </div>
+        {/* End plan 1 */}
 
-
-
+        {/* Plan 2 */}
         <div className="group relative bg-white dark:bg-black rounded-lg shadow-solid-10 dark:shadow-none border border-stroke dark:border-black p-7.5 xl:p-12.5">
           <h2 className="mb-5 text-[32px] font-bold text-black dark:text-white">
             Professional Plan
@@ -365,9 +302,9 @@ async function Page() {
             Activate
           </button>
         </div>
+        {/* End plan 2 */}
 
-
-
+        {/* Plan 3 */}
         <div className="group relative bg-white dark:bg-black rounded-lg shadow-solid-10 dark:shadow-none border border-stroke dark:border-black p-7.5 xl:p-12.5">
           <h2 className="mb-5 text-[32px] font-bold text-black dark:text-white">
             Enterprise Plan
@@ -435,8 +372,7 @@ async function Page() {
             Activate
           </button>
         </div>
-        
-
+        {/* End plan 3 */}
       </div>
     </>
   )
