5a34d2989bcfe95da953a32bac17769b9b0ecb6e
jaysoncano <cano.jayson10@gmail.com>
2023-12-07 09:29:57 +0800

    Style: Create template invoice table summary

--- app/invoices/details/[invoice_id]/page.tsx
+++ app/invoices/details/[invoice_id]/page.tsx
@@ -1,7 +1,12 @@
+import Invoice from "@/components/Invoice/Invoice";
 import React from "react";
 
 const page = () => {
-  return <div>Invoice Details Page</div>;
+  return (
+    <>
+      <Invoice />
+    </>
+  );
 };
 
 export default page;
---
+++ components/Invoice/Invoice.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const Invoice = () => {
+  return (
+    <>
+      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+          <h3 className="font-medium text-black dark:text-white">
+            Invoice Summary
+          </h3>
+        </div>
+        <div className="flex flex-col gap-5.5 p-6.5">
+          {/* Start:: Notification if Successfully Paid */}
+          <div className="mb-4 hidden">
+            <div className="flex w-full rounded-lg border-l-[6px] border-[#34D399] bg-[#34D399] bg-opacity-[15%] px-7 py-8 shadow-md md:p-9">
+              <div className="mr-5 flex h-9 w-full max-w-[36px] items-center justify-center rounded-lg bg-[#34D399]">
+                <svg
+                  width="16"
+                  height="12"
+                  viewBox="0 0 16 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15.2984 0.826822L15.2868 0.811827L15.2741 0.797751C14.9173 0.401867 14.3238 0.400754 13.9657 0.794406L5.91888 9.45376L2.05667 5.2868C1.69856 4.89287 1.10487 4.89389 0.747996 5.28987C0.417335 5.65675 0.417335 6.22337 0.747996 6.59026L0.747959 6.59029L0.752701 6.59541L4.86742 11.0348C5.14445 11.3405 5.52858 11.5 5.89581 11.5C6.29242 11.5 6.65178 11.3355 6.92401 11.035L15.2162 2.11161C15.5833 1.74452 15.576 1.18615 15.2984 0.826822Z"
+                    fill="white"
+                    stroke="white"
+                  ></path>
+                </svg>
+              </div>
+              <div className="w-full">
+                <h5 className="mb-3 text-lg font-semibold text-dark">
+                  {"Paid Successfully"}
+                </h5>
+              </div>
+            </div>
+          </div>
+          {/* End:: Notification if Successfully paid */}
+
+          {/* Start:: Table */}
+          <div className="w-full table-auto">
+            <table className="w-full table-auto">
+              <thead>
+                <tr className="bg-gray-2 text-left dark:bg-meta-4">
+                  <th className="min-w-[220px] table-th-header">Plan</th>
+                  <th className="min-w-[150px] table-th-header">Amount</th>
+                  <th className="min-w-[120px] table-th-header">Additional</th>
+                  <th className="min-w-[120px] table-th-header">Status</th>
+                  <th className="min-w-[120px] table-th-header">Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <h5 className="font-medium text-black dark:text-white">
+                      Basic Free
+                    </h5>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <h5 className="font-medium text-black dark:text-white">
+                      $99.00
+                    </h5>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <h5 className="font-medium text-black dark:text-white">
+                      $0.10
+                    </h5>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <div className="inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium text-success bg-success">
+                      Due
+                    </div>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <div className="inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium text-success bg-success">
+                      Dec. 17, 2023
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {/* End:: Table */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Invoice;
